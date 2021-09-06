4fabc211995391d4f18361049a8017b1687ddbe2
Bruno Kurt <brunojkurt@gmail.com>
2021-09-06 17:00:52 -0300

    improving slider

--- components/Swiper/index.jsx
+++ components/Swiper/index.jsx
@@ -1,64 +1,24 @@
-import { Children, useRef, useEffect, useState } from 'react'
+import React, { Children } from 'react'
+import Slider from './Slider'
+import { SwiperContainer } from './styles'
 
-import {
-  SwiperContainer,
-  SwipeableContainer
-} from './styles'
-
-const Swiper = ({ children }) => {
-  const containerRef = useRef(null)
-  const contents = Children.toArray(children)
-
-  const [pointerState, setPointerState] = useState({
-    down: false,
-    startX: 0,
-    containerScroll: 0
-  })
-  const [measures, setMeasures] = useState({
-    swipeable: {
-      width: 0
-    }
-  })
-
-  useEffect(() => {
-    if (containerRef.current) {
-      setMeasures(state => ({
-        ...state,
-        swipeable: {
-          ...state.swipeable,
-          width: containerRef.current.offsetWidth
-        }
-      }))
-    }
-  }, [containerRef.current])
-
-  const handleMovement = (e) => {
-    if (pointerState.down) {
-      const dx = e.clientX - pointerState.startX;
-      containerRef.current.scrollLeft = pointerState.containerScroll - dx
-    }
-  }
+const Swiper = ({ children, showButtons, showCounter, visible }) => {
+  const contents = Children.toArray(children).map(content => ({ content }))
 
   return (
-    <SwiperContainer
-      onMouseDown={(e) => setPointerState({
-        down: true,
-        startX: e.clientX,
-        containerScroll: containerRef.current.scrollLeft
-      })}
-      onMouseUp={() => setPointerState(state => ({ ...state, down: false, startX: 0 }))}
-      onMouseMove={handleMovement}
-      ref={containerRef}
-      pointerDown={pointerState.down ? 'true' : false}
-    >
-      {contents.map((content, i) => (
-        <SwipeableContainer
-          key={i}
-          width={measures.swipeable.width}
-        >
-          {content}
-        </SwipeableContainer>
-      ))}
+    <SwiperContainer>
+      <Slider
+        items={contents}
+        showButtons={showButtons}
+        showCounter={showCounter}
+        visible={visible}
+      >
+        {({ content }, i) => (
+          <div key={i}>
+            {content}
+          </div>
+        )}
+      </Slider>
     </SwiperContainer>
   )
 }
--- pages/swiper.jsx
+++ pages/swiper.jsx
@@ -3,9 +3,11 @@ import Swiper from '../components/Swiper'
 
 const Container = styled.div`
   width: 100%;
-  padding-left: 10vw;
-  padding-right: 10vw;
-  margin-top: 5vh;
+  height: 800px;
+`
+const SwiperItem = styled.div`
+  background: ${({ color }) => color};
+  width: 100%;
   height: 800px;
 `
 
@@ -13,9 +15,9 @@ const SwiperPage = () => {
   return (
     <Container>
       <Swiper>
-        <h2>Slider 1</h2>
-        <h2>Slider 2</h2>
-        <h2>Slider 3</h2>
+        <SwiperItem color="red">Slider 1</SwiperItem>
+        <SwiperItem color="blue">Slider 2</SwiperItem>
+        <SwiperItem color="grey">Slider 3</SwiperItem>
       </Swiper>
     </Container>
   )
